0c766079b456b31e52a76dc119f0c78db4698075
Diego <diegosebastian_t@hotmail.com>
2024-11-11 00:40:39 -0500

    Implementación de Login funcional

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -73,4 +73,56 @@ export const server = {
       }
     }
   }),
+  loginUser: defineAction({
+    accept: "form",
+    input: z.object({
+      email: z.string().email(),
+      password: z.string().min(8, { 
+        message: "Password must be at least 8 characters long" 
+      }),
+    }),
+    handler: async (loginData) => {
+      try {
+        // Convertir el objeto loginData al formato esperado por el backend
+        const loginPayload = {
+          email: loginData.email,
+          contrasenia: loginData.password,
+        };
+
+        // Realizar la petición POST a Spring Boot
+        const response = await fetch('http://localhost:8080/usuario/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(loginPayload),
+        });
+
+        // Si la petición no fue exitosa, se retorna un objeto con el error
+        if (!response.ok) {
+          return { success: false, error: { fields: { general: 'Incorrect email or password' } } };
+        }
+
+        // Leer la respuesta como texto y convertirla a booleano
+        const text = await response.text();
+        const data = text === 'true';
+
+        // Si el login fue exitoso, redirigir al usuario
+        if (data) {
+          return { success: true };
+        } else {
+          return { success: false, error: { fields: { general: 'Incorrect email or password' } } };
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error(error);
+          // Manejar el error, por ejemplo, mostrar un mensaje de error al usuario
+          return { success: false, error: { fields: { general: error.message } } };
+        } else {
+          console.error("Unknown error", error);
+          return { success: false, error: { fields: { general: "Unknown error" } } };
+        }
+      }
+    }
+  }),
 };
--- src/components/Forms/LoginForm.tsx
+++ src/components/Forms/LoginForm.tsx
@@ -1,74 +1,151 @@
-import React from 'react';
+import { actions } from 'astro:actions';
+import React, { useState } from 'react';
+import type { FormEvent } from 'react';
 import { AiOutlineLock } from 'react-icons/ai';
 
 const LoginForm: React.FC = () => {
-    return (
-        <div className="max-w-md w-full space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to your account
-          </h2>
-        </div>
-        <form className="mt-8 space-y-6" action="/dashboard" method="GET">
-          <div className="rounded-md shadow-sm space-y-4">
-            <div>
-              <label htmlFor="email" className="sr-only">Email address</label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                required
-                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:z-10 sm:text-sm"
-                placeholder="Email address"
-              />
-            </div>
-            <div>
-              <label htmlFor="password" className="sr-only">Password</label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                required
-                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:z-10 sm:text-sm"
-                placeholder="Password"
-              />
-            </div>
-          </div>
-  
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                id="remember-me"
-                name="remember-me"
-                type="checkbox"
-                className="h-4 w-4 text-primary-600 focus:ring-primary-500 border-gray-300 rounded"
-              />
-              <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                Remember me
-              </label>
-            </div>
-  
-            <div className="text-sm">
-              <a href="#" className="font-medium text-primary-600 hover:text-primary-500">
-                Forgot your password?
-              </a>
-            </div>
-          </div>
-  
-          <div>
-            <button
-              type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
-            >
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                <AiOutlineLock size={25} />
-              </span>
-              Sign in
-            </button>
-          </div>
-        </form>
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setErrors({});
+
+    const formDataObj = new FormData(e.currentTarget);
+
+    const result = await actions.loginUser(formDataObj);
+
+    if (result.error) {
+      console.error(result.error);
+
+      const errorMessages: { [key: string]: string } = {};
+      if (result.error && 'fields' in result.error) {
+        Object.entries(result.error.fields as { [key: string]: string[] }).forEach(([key, value]) => {
+          errorMessages[key] = value[0];
+        });
+      }
+      setErrors(errorMessages);
+      return;
+    }
+
+    if (!result.data.success) {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          general: result.data.error?.fields.general ?? '',
+        }));
+      return;
+    }
+
+    // Redirigir al usuario a la página de dashboard
+    window.location.href = '/dashboard';
+  };
+
+  return (
+    <div className="max-w-md w-full space-y-8">
+      <div>
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Sign in to your account
+        </h2>
       </div>
-    );
-}
+      <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+        <div className="rounded-md shadow-sm space-y-4">
+          <div>
+            <label htmlFor="email" className="sr-only">
+              Email address
+            </label>
+            <input
+              id="email"
+              name="email"
+              
+              required
+              value={formData.email}
+              onChange={handleChange}
+              className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:z-10 sm:text-sm"
+              placeholder="Email address"
+            />
+            {errors.email && (
+              <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-3" role="alert">
+                <span className="block sm:inline">{errors.email}</span>
+              </div>
+            )}
+          </div>
+          <div>
+            <label htmlFor="password" className="sr-only">
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              required
+              value={formData.password}
+              onChange={handleChange}
+              className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:z-10 sm:text-sm"
+              placeholder="Password"
+            />
+            {errors.password && (
+              <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-3" role="alert">
+                <span className="block sm:inline">{errors.password}</span>
+              </div>
+            )}
+          </div>
+        </div>
+
+        {errors.general && (
+          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-5" role="alert">
+            <span className="block sm:inline">{errors.general}</span>
+          </div>
+        )}
+
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <input
+              id="remember-me"
+              name="remember-me"
+              type="checkbox"
+              className="h-4 w-4 text-primary-600 focus:ring-primary-500 border-gray-300 rounded"
+            />
+            <label
+              htmlFor="remember-me"
+              className="ml-2 block text-sm text-gray-900"
+            >
+              Remember me
+            </label>
+          </div>
+
+          <div className="text-sm">
+            <a
+              href="#"
+              className="font-medium text-primary-600 hover:text-primary-500"
+            >
+              Forgot your password?
+            </a>
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+          >
+            <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+              <AiOutlineLock size={25} />
+            </span>
+            Sign in
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default LoginForm;
